395696503f16f2a849d3a2bfb90f4c9feea8a61d
Frederic Ruyer <frederic.ruyer@ac-grenoble.fr>
2020-06-10 14:51:52 +0200

    premiere version

---
+++ indexation.py
@@ -0,0 +1,33 @@
+# Mon TP
+# Fred Ruyer
+
+import outils.py
+
+
+def doc2listeRacines(chemin):
+    """
+
+    :param chemin: chemin du document
+    :return: liste des racines
+    """
+    return[]
+
+
+def calculeTF(listeracine):
+    """
+    :param listeracine: dictionnaire de cles noms desdocs et d'éléments la liste des racines
+    :return: dictionnaire de clés nm du doc et d'éléments un dictionnaire de clé la racine et d'élément son tf
+    """
+    return{}
+
+
+
+
+
+
+
+
+
+
+
+
